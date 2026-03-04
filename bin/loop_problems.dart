d3193b96dc6121f725ac224d7b627b681bb20b1b
mohamedgomaa20 <20monagy@gmail.com>
2026-03-04 03:47:54 +0000

    Q2- Add methods to print odd numbers and rename even numbers method

--- bin/loop_problems.dart
+++ bin/loop_problems.dart
@@ -1,8 +1,17 @@
 class LoopProblems {
   ///Q1- Print Even Numbers
-  void printEvenNumber(int number) {
+  void printEvenNumbers(int number) {
     for (int i = 1; i <= number; i++) {
       if (i % 2 == 0) {
+        print(i);
+      }
+    }
+  }
+
+  ///Q2- Print Odd Numbers
+  void printOddNumbers(int number) {
+    for (int i = 1; i <= number; i++) {
+      if (i % 2 != 0) {
         print(i);
       }
     }
--- bin/nti_task_two.dart
+++ bin/nti_task_two.dart
@@ -2,7 +2,10 @@ import 'loop_problems.dart';
 
 void main(List<String> arguments) {
   LoopProblems problems = LoopProblems();
-  ///Q1- Print Even Numbers
-  problems.printEvenNumber(10);
 
+  ///Q1- Print Even Numbers
+  problems.printEvenNumbers(10);
+
+  ///Q1- Print Odd Numbers
+  problems.printOddNumbers(10);
 }
