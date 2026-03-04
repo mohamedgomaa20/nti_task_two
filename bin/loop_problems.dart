9f97b174721339d05ff8abc81696d026a96efe2c
mohamedgomaa20 <20monagy@gmail.com>
2026-03-04 03:45:11 +0000

    Q1- Add LoopProblems class with method to print even numbers

---
+++ bin/loop_problems.dart
@@ -0,0 +1,10 @@
+class LoopProblems {
+  ///Q1- Print Even Numbers
+  void printEvenNumber(int number) {
+    for (int i = 1; i <= number; i++) {
+      if (i % 2 == 0) {
+        print(i);
+      }
+    }
+  }
+}
---
+++ bin/nti_task_two.dart
@@ -0,0 +1,8 @@
+import 'loop_problems.dart';
+
+void main(List<String> arguments) {
+  LoopProblems problems = LoopProblems();
+  ///Q1- Print Even Numbers
+  problems.printEvenNumber(10);
+
+}
